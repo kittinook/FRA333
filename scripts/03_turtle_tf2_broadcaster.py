c7bad60274c0e920fcef939e700688585c0e5313
kittinook <kitti.thamrong@gmail.com>
2022-08-17 23:29:08 +0700

    Update 03_turtle_tf2_broadcaster.py

--- scripts/03_turtle_tf2_broadcaster.py
+++ scripts/03_turtle_tf2_broadcaster.py
@@ -34,31 +34,7 @@ class FramePublisher(Node):
         self.subscription  # prevent unused variable warning
 
     def handle_turtle_pose(self, msg):
-        t = TransformStamped()
-
-        # Read message content and assign it to
-        # corresponding tf variables
-        t.header.stamp = self.get_clock().now().to_msg()
-        t.header.frame_id = 'world'
-        t.child_frame_id = self.turtlename
-
-        # Turtle only exists in 2D, thus we get x and y translation
-        # coordinates from the message and set the z coordinate to 0
-        t.transform.translation.x = msg.x
-        t.transform.translation.y = msg.y
-        t.transform.translation.z = 0.0
-
-        # For the same reason, turtle can only rotate around one axis
-        # and this why we set rotation in x and y to 0 and obtain
-        # rotation in z axis from the message
-        q = tf_transformations.quaternion_from_euler(0, 0, msg.theta)
-        t.transform.rotation.x = q[0]
-        t.transform.rotation.y = q[1]
-        t.transform.rotation.z = q[2]
-        t.transform.rotation.w = q[3]
-
-        # Send the transformation
-        self.br.sendTransform(t)
+        pass
 
 
 def main():
