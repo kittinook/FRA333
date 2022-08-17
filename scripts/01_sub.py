a78155fc6778dfbf1740bf6b6096894402c97549
kittinook <kitti.thamrong@gmail.com>
2022-08-17 23:26:39 +0700

    Update 01_sub.py

--- scripts/01_sub.py
+++ scripts/01_sub.py
@@ -2,28 +2,24 @@
 import rclpy
 from rclpy.node import Node
 
-from geometry_msgs.msg import Twist
+# EDIT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 class SubscriberClass(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(
-            Twist,
-            '/turtle1/cmd_vel',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
+        super().__init__('subscriber')
+        # EDIT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     def listener_callback(self, msg):
-        print(msg)
+        # EDIT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        pass
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    subscriber = SubscriberClass()
+    # EDIT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     rclpy.spin(subscriber)
 
